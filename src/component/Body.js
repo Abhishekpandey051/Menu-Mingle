029dd0e5e5bb7687d205ba49a089e19f205c7c7e
Abhishekpandey051 <95233418+Abhishekpandey051@users.noreply.github.com>
2023-12-21 00:17:54 +0530

    add searching functionality

--- src/component/Body.js
+++ src/component/Body.js
@@ -3,6 +3,7 @@ import Card from "./Card";
 
 export default function Body() {
   const [listOfRest, setListOfRest] = useState([]);
+  const [searcCard, setSearchCard] = useState([]);
 
   useEffect(() => {
     fetchAndSetData();
@@ -14,7 +15,7 @@ export default function Body() {
         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const data = await response.json();
-      console.log(data.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
+      // console.log(data.data.cards[2].card.card.gridElements.infoWithStyle.restaurants)
       const resdata =
       data.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
       setListOfRest(resdata);
@@ -23,11 +24,17 @@ export default function Body() {
     }
   };
   const topResto = () =>{
-    
+    setListOfRest(listOfRest.filter((res)=>res.info.avgRating>4.2))
+  }
+  const searchRes = () =>{
+    const searchName = listOfRest.filter((resName)=>resName.info.name.toLowerCase().includes(searcCard.toLowerCase()))
+    setListOfRest(searchName)
   }
   return (
     <div className="mx-3 sticky z-50 block ">
-    <button onClick={topResto} className="m-3 bg-gray-900 text-white font-bold">Top Rated Reastaurant</button>
+    <button onClick={topResto} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2">Top Restaurant</button>
+    <input onChange={(e)=>{setSearchCard(e.target.value)}} type="text" className="border bg-gray-400"/>
+    <button onClick={searchRes} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ">Search</button>
       <h1 className=" text-center text-2xl font-bold mt-4">Your Restaurant</h1>
       
       {listOfRest.map((val) => (
