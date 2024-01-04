f80ce42f93887241f7706bc6aa0d47c43cd7c7c2
Abhishekpandey051 <95233418+Abhishekpandey051@users.noreply.github.com>
2024-01-04 20:48:16 +0530

    changes search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import Navbar from "./component/Navbar";
 import Body from "./component/Body";
 import About from "./component/About";
+import Contact from "./component/Contact"
+import { createBrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="relative ">
       <Navbar />
       <Body />
-      <About/>
     </div>
   );
 }
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+  },
+  {
+  path: "/about",
+  element:<About/>,
+  },
+  {
+    path: "/contact",
+    element:<Contact/>
+  }
+]);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { appRouter } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <App/>
+    <RouterProvider router={appRouter}/>
   
 )
 
